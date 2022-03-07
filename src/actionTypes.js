fb1e313aed45ab952b7b0cc63b66d2640317d1ae
Hamim <rahmanhamiminfo@gmail.com>
2022-03-07 11:10:44 +0600

    redux with mosh

---
+++ src/actionTypes.js
@@ -0,0 +1,3 @@
+// actionTypes.js
+export const BUG_ADDED = "bugAdded";
+export const BUG_REMOVED = "bugRemove";
--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+// index.js
+
+import store from "./store";
+import * as actions from "./actionTypes";
+
+const unsubscribe = store.subscribe(() => {
+ console.log("Store changed!", store.getState());
+});
+
+store.dispatch({
+ type: actions.BUG_ADDED,
+ payload: {
+  description: "Bug1",
+ },
+});
+
+unsubscribe();
+
+store.dispatch({
+ type: actions.BUG_REMOVED,
+ payload: {
+  id: 1,
+ },
+});
+
+console.log(store.getState());
